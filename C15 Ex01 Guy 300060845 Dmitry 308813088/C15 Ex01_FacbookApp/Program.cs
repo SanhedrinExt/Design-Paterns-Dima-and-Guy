c8f02d1e41d56c6113b5aa7202f2ac55a35ee9e9
Gezer <guydunski@gmail.com>
2015-08-03 16:56:38 +0300

    New Project

---
+++ C15 Ex01 Guy 300060845 Dmitry 308813088/C15 Ex01_FacbookApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace C15_Ex01_FacbookApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
